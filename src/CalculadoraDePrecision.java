bef96744dbc22bfba43c9d022dd213a8e0eb66b7
YordyACH <yordyalanch1103@gmail.com>
2025-12-19 16:29:46 -0600

    Aprendiendo precision en java

---
+++ src/CalculadoraDePrecision.java
@@ -0,0 +1,8 @@
+public class CalculadoraDePrecision {
+   public static void main(String[] args) {
+        float f = 1.123456789f;
+        double d = 1.123456789;
+       System.out.println(f);
+       System.out.println(d);
+    }
+}
---
+++ src/LimiteDatos.java
@@ -0,0 +1,7 @@
+public class LimiteDatos {
+    public static void main(String[] args) {
+        byte prueba = 127;
+        int suma = prueba + 1;
+        System.out.println(suma);
+    }
+}
